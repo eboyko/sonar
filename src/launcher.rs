cc45a8641413b9e53e2993f63a52b30715a2a3ca
Evgeny Boyko <eboyko@eboyko.ru>
2024-04-08 02:38:49 +0300

    Add launch script

--- src/launcher.rs
+++ src/launcher.rs
@@ -1,13 +1,12 @@
+use std::fmt::Error;
 use std::sync::Arc;
 
-use log::{debug, warn};
 use signal_hook::consts::{SIGINT, SIGTERM};
 use signal_hook::iterator::Signals;
 use tokio_util::sync::CancellationToken;
 use tokio_util::task::TaskTracker;
 
 use crate::{listener, monitor, recorder, settings};
-use crate::error::Error;
 
 pub(crate) struct Launcher {
     threads: TaskTracker,
@@ -18,40 +17,43 @@ impl Launcher {
     pub async fn listen_signals(&self) {
         let context = self.context.clone();
 
-        self.threads.spawn(async move {
-            let mut signals = Signals::new([SIGINT, SIGTERM]).unwrap();
-            debug!("Termination signal monitoring initiated");
-
-            for signal in signals.forever() {
-                warn!("Termination signal {} received", signal);
-                context.cancel();
-                return;
-            }
-        });
-    }
-
-    pub async fn stop(&self) {
-        self.threads.close();
-        self.threads.wait().await;
+        let mut signals = Signals::new([SIGINT, SIGTERM]).unwrap();
+        if signals.forever().next().is_some() {
+            context.cancel();
+            self.threads.close();
+        }
     }
 }
 
 pub(crate) async fn start() -> Result<(), Error> {
-    let launcher = Launcher {
+    let launcher = Arc::new(Launcher {
         threads: TaskTracker::new(),
         context: CancellationToken::new(),
-    };
+    });
 
     let settings = settings::build()?;
-    let recorder = recorder::build(Arc::clone(&settings))?;
-    let mut monitor = monitor::build(Arc::clone(&recorder), launcher.context.clone()).await?;
-    let mut listener = listener::build(Arc::clone(&settings), Arc::clone(&recorder), launcher.context.clone());
 
-    launcher.listen_signals().await;
+    let recorder = recorder::build(settings.path.clone())?;
+
+    let listener = listener::build(
+        settings.url.clone(),
+        settings.timeout,
+        Arc::clone(&recorder),
+        launcher.context.clone(),
+    );
+
+    let monitor = monitor::build(
+        Arc::clone(&listener),
+        Arc::clone(&recorder),
+        launcher.context.clone(),
+    ).await.unwrap();
+
+    let launcher_reference = Arc::clone(&launcher);
+    launcher.threads.spawn(async move { listener.start().await });
     launcher.threads.spawn(async move { monitor.start().await });
-    launcher.threads.spawn(async move { listener.listen_stream().await });
+    launcher.threads.spawn(async move { launcher_reference.listen_signals().await; });
 
-    launcher.stop().await;
+    launcher.threads.wait().await;
 
     Ok(())
 }
