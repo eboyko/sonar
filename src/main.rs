88f7ddbf08fd3115ed59d6f164425d2de52c35ac
Evgeny Boyko <eboyko@eboyko.ru>
2024-04-08 02:39:19 +0300

    Prettify main file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 use log::error;
 
-use crate::error::Error;
-use crate::launcher::Launcher;
-
-mod command_line_parser;
-mod error;
+mod launcher;
 mod listener;
+mod monitor;
 mod recorder;
 mod settings;
-mod monitor;
-mod launcher;
 
 #[tokio::main]
 async fn main() {
@@ -17,6 +12,5 @@ async fn main() {
 
     if let Err(error) = launcher::start().await {
         error!("{}", error);
-        return;
     }
 }
