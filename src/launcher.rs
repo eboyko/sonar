8046e283bdc591aa83040835445128a9ca66b5a5
Evgeny Boyko <eboyko@eboyko.ru>
2024-04-09 18:19:13 +0300

    Implement new settings to launch the thing

--- src/launcher.rs
+++ src/launcher.rs
@@ -1,13 +1,14 @@
+use log::{info, warn};
 use std::error::Error;
 use std::sync::Arc;
-use log::{error, info, warn};
 
 use signal_hook::consts::{SIGINT, SIGTERM};
 use signal_hook::iterator::Signals;
 use tokio_util::sync::CancellationToken;
 use tokio_util::task::TaskTracker;
 
-use crate::{listener, monitor, recorder, settings};
+use crate::settings::Settings;
+use crate::{listener, monitor, recorder};
 
 pub(crate) struct Launcher {
     threads: TaskTracker,
@@ -19,7 +20,7 @@ impl Launcher {
         let context = self.context.clone();
         let mut signals = Signals::new([SIGINT, SIGTERM]).unwrap();
         info!("Listening for termination signals.");
-        
+
         if signals.forever().next().is_some() {
             warn!("Termination signal received. Shutting down.");
             context.cancel();
@@ -28,40 +29,34 @@ impl Launcher {
     }
 }
 
-pub(crate) async fn start() -> Result<(), Box<dyn Error>> {
+pub(crate) async fn start(settings: Settings) -> Result<(), Box<dyn Error>> {
     let launcher = Arc::new(Launcher {
         threads: TaskTracker::new(),
         context: CancellationToken::new(),
     });
 
-    let settings = match settings::build() {
-        Ok(settings) => settings,
-        Err(error) => return Err(Box::new(error)),
-    };
-
-    let recorder = recorder::build(settings.path.clone())?;
+    let recorder = recorder::build(settings.records_path)?;
 
     let listener = listener::build(
-        settings.url.clone(),
-        settings.timeout,
+        settings.stream_url,
+        settings.stream_timeout,
         Arc::clone(&recorder),
         launcher.context.clone(),
     );
 
     let monitor = monitor::build(
-        settings.port,
+        settings.monitor_port_number,
         Arc::clone(&listener),
         Arc::clone(&recorder),
         launcher.context.clone(),
     );
 
     let launcher_reference = Arc::clone(&launcher);
+
     launcher.threads.spawn(async move { launcher_reference.listen_signals().await });
     launcher.threads.spawn(async move { listener.start().await });
     launcher.threads.spawn(async move { monitor.start().await });
-
     launcher.threads.wait().await;
 
     Ok(())
 }
-
