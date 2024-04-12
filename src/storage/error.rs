1eccb0a9f5d05fcd05f6c3a4ceddbc1f8664e13a
Evgeny Boyko <eboyko@eboyko.ru>
2024-04-12 04:43:39 +0300

    Move recorder errors into storage crate

--- src/storage/error.rs
+++ src/storage/error.rs
@@ -3,13 +3,21 @@ use std::fmt::{Display, Formatter, Result};
 #[derive(Debug)]
 pub(crate) enum Error {
     ActiveDiskDetectionFailed,
+    OperationFailed(std::io::Error),
 }
 
 impl Display for Error {
     fn fmt(&self, formatter: &mut Formatter) -> Result {
         match self {
             Error::ActiveDiskDetectionFailed => write!(formatter, "Could not detect an active disk for the provided path"),
+            Error::OperationFailed(error) => write!(formatter, "I/O operation failed: {}", error),
         }
+    }
+}
+
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Error::OperationFailed(error)
     }
 }
 
